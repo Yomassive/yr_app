4c61daf2523e05ece4d83b9f77df65b223453b80
Yomassive <yomassive.andrew@yandex.ru>
2018-12-03 14:53:50 +0400

    updated timing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ setInterval(function(){
   client.post('statuses/update', {status: status+' '+new Date().getMilliseconds()},  function(error, tweet, response) {
     if(error) throw error;
   });
-}, 10*60*1000);
+}, 30*60*1000);//minutes*seconds*mseconds
 
 // var params = {screen_name: 'Portal_63'};
 // client.get('statuses/user_timeline', params, function(error, tweets, response) {
